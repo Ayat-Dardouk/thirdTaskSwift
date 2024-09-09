15f93dfe31b33154abb75b953a09508f162b8710
WasanAl-Sayed <wasanjehad75@gmail.com>
2024-09-09 14:48:34 +0300

    thirdTask : setup project

---
+++ thirdTask/thirdTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thirdTask
+//
+//  Created by fts on 09/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
